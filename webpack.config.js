e37464d601cdf14d9db77423b64bb08fdc56d6d5
Ambre Heitzmann <ambre@MacBook-de-Romain.local>
2018-11-05 16:58:42 +0100

    some changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
